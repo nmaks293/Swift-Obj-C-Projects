550f3f7ce7ef8efef935ca48559fd9df8c9ac14c
rokirovochka <nmaksakovskii@gmail.com>
2019-10-24 19:00:38 +0700

    Initial Commit

---
+++ CarShop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CarShop
+//
+//  Created by Никита Максаковский on 24.10.2019.
+//  Copyright © 2019 Никита Максаковский. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
